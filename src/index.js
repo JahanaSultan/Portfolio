7fcf97da595b012bf9ca54cc5fdf3628c7b7e97f
JahanaSultan <cahana.aliyeva@gmail.com>
2022-07-28 02:53:14 +0400

    bug fixed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/Portfolio'>
+  <BrowserRouter basename={window.location.pathname || ''}>
     <App />
   </BrowserRouter>
 );
